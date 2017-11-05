be8ca44a6d34509826ea4010fdd6dbcf5a681bd9
gablooblue <gabrielhenry.lopez@gmail.com>
2017-11-05 18:41:24 +0800

    Added file reading and adjency matrix

--- src/exam-scheduler.py
+++ src/exam-scheduler.py
@@ -1,13 +1,37 @@
-def read(classes, student_nos, student_names):
+def read(classes, students):
     CLASSES_PATH = "../input/classes.txt"
+    STUDENTS_PATH = "../input/students.txt"
 
     class_file = open(CLASSES_PATH, "r")
-    classes = [class_file.readline().strip() for i in class_file]
+    for count, i in enumerate(class_file.readlines()):
+        COURSE_PATH = "../input/COURSE" + str(count + 1) + ".txt"
+        course_file = open(COURSE_PATH, "r")
+        course = []
+
+        for j in course_file.readlines():
+            course.append(j.strip())
+
+        course_file.close()
+        classes.append(course)
+    class_file.close() 
+
+    students_file = open(STUDENTS_PATH, "r")
+
+    for i in students_file.readlines():
+        students.append(i.strip().split(","))
 
 def main():
     classes = []
-    student_nos = []
-    student_names = []
-    read(classes, student_nos, student_names)
+    students = []
+    read(classes, students)
+    edges = [[0 for j in i] for i in classes]
+
+    for i in range(len(classes)):
+        for j in range(i + 1, len(classes)):
+            for k in classes[i]:
+                if k in classes[j]:
+                    edges[i][j] = 1
+
+    print(edges)
     
 main()
